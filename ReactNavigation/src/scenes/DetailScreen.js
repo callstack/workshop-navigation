4590a9eb0d3f2e3cd7cd0e9c9021da9b16eda6f3
ferrannp <fnp.developer@gmail.com>
2017-08-26 20:03:55 +0200

    Push screen part 2.

--- ReactNavigation/src/scenes/DetailScreen.js
+++ ReactNavigation/src/scenes/DetailScreen.js
@@ -1,17 +1,27 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, View, Platform, Text, StyleSheet } from 'react-native';
 
 export default class DetailScreen extends Component {
   static navigationOptions = {
     title: 'Detail'
   };
 
+  goBack = () => {
+    const { navigation } = this.props;
+    navigation.goBack();
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Look ma! I'm another screen</Text>
+        <Button
+          color={Platform.OS === 'android' ? "#3F51B5" : "#007aff"}
+          onPress={this.goBack}
+          title="Pop screen"
+        />
       </View>
     )
   }
