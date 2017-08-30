537284e94d7c57dc9b5210dec631ca8badf93034
ferrannp <fnp.developer@gmail.com>
2017-08-30 15:11:51 +0200

    Redux part 2: Handle Deep Linking.

--- ReactNavigation/src/AppContainer.js
+++ ReactNavigation/src/AppContainer.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Linking, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 
 import configureStore from './redux/configureStore';
@@ -10,7 +10,48 @@ import App, { AppNavigator } from './App';
 const store = configureStore(AppNavigator);
 
 class Root extends Component {
+  state = {
+    isLinkingChecked: false,
+  };
+
+  componentDidMount() {
+    const { isLinkingChecked } = this.state;
+    if (!isLinkingChecked) {
+      Linking.getInitialURL().then(url => {
+        this.handleLinking(url);
+        this.setState({ isLinkingChecked: true });
+      });
+    }
+
+    if (Platform.OS === 'ios') {
+      Linking.addEventListener('url', this.handleUrlIos);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'ios') {
+      Linking.removeEventListener('url', this.handleUrlIos);
+    }
+  }
+
+  handleUrlIos = (event) => {
+    this.handleLinking(event.url);
+  };
+
+  handleLinking = (url) => {
+    if (url) {
+      store.dispatch({
+        type: 'LINKING',
+        url,
+      });
+    }
+  };
+
   render() {
+    const { isLinkingChecked } = this.state;
+    if (!isLinkingChecked) {
+      return null;
+    }
     return (
       <Provider store={store}>
         <App />
--- ReactNavigation/src/redux/modules/navigation.js
+++ ReactNavigation/src/redux/modules/navigation.js
@@ -1,18 +1,33 @@
 /* @flow */
 
+import { Platform } from 'react-native';
 import { NavigationActions } from 'react-navigation'
 
-const createReducer = (AppNavigator) => {
-  const initialState = AppNavigator.router.getStateForAction(
+const _getInitialState = (AppNavigator, actions) => {
+  return AppNavigator.router.getStateForAction(
     NavigationActions.reset({
-      index: 0,
-      actions: [
-        NavigationActions.navigate({ routeName: 'Home' }),
-      ]}
-    )
+      index: actions.length - 1,
+      actions: actions.map(action => NavigationActions.navigate(action)),
+    })
   );
+};
+
+const prefix = Platform.OS === 'android' ? 'navigation://navigation/' : 'navigation://';
+
+const createReducer = (AppNavigator) => {
+  // Initial route we want for our app
+  const initialState = _getInitialState(AppNavigator, [{ routeName: 'Home' }]);
 
   return (state = initialState, action) => {
+    // We handle special deep linking cases
+    if(action.type === 'LINKING' && action.url.includes(`${prefix}home/detail/`)) {
+      const id = action.url.split(`${prefix}home/detail/`)[1];
+      return _getInitialState(
+        AppNavigator,
+        [{ routeName: 'Home' }, { routeName: 'Detail', params: { id }}]
+      );
+    }
+    // Otherwise default
     const newState = AppNavigator.router.getStateForAction(action, state);
     return newState || state;
   };
